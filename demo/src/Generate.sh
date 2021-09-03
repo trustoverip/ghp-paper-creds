8dd5084e596176cc95bd3ba491ceafe8513c87bc
David Janes <david@consensas.com>
2021-09-03 11:02:04 -0400

    don't overwrite
    Signed-off-by: Vitor Pamplona <vitor@vitorpamplona.com>

--- demo/src/Generate.sh
+++ demo/src/Generate.sh
@@ -4,6 +4,8 @@ for file in example-antigen example-recovery example-pass example-vaccination
 do
     echo "== $file =="
 
+    [ -f $FOLDER/$file-vc.json ] && continue
+
     node demo.js \
         --credential $FOLDER/$file.json \
         --vc $FOLDER/$file-vc.json \
